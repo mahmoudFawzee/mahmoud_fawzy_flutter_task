9db5bf43df4c7eba1c58be06daa47cbf99bb1eff
mahmoudFawzee <mahmoudfawzee1511@gmail.com>
2025-10-18 08:35:54 +0300

    hotfix: remove unneeded items

--- lib/features/filters/cubit/filters_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'filters_state.dart';
-
-class FiltersCubit extends Cubit<FiltersState> {
-  FiltersCubit() : super(FiltersInitial());
-}
